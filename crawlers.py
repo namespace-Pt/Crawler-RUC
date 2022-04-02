201f7a388d94de0aff2c4d5a7cd836ca1c65ce2a
namespace-Pt <namespace.pt@gmail.com>
2022-04-03 00:01:54 +0800

    1

--- crawlers.py
+++ crawlers.py
@@ -5,6 +5,25 @@ from bs4 import BeautifulSoup
 from utils import *
 
 
+def get_headers(crawler_type):
+    user_agent_pool = [
+        'Mozilla/5.0 (X11; CrOS i686 4319.74.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10',
+        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:23.0) Gecko/20131011 Firefox/23.0',
+        'Mozilla/5.0 (Windows; U; Windows NT 6.0; hu-HU) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
+    ]
+    accept_map = {
+        "vruc": "application/xhtml+xml,text/plain,*/*",
+        "jw": "application/json,text/plain,*/*"
+    }
+    headers = {
+        "User-Agent": random.choice(user_agent_pool),
+        "Accept": accept_map[crawler_type]
+    }
+    return headers
+
 
 class VRUCCrawler():
     def __init__(
@@ -13,7 +32,7 @@ class VRUCCrawler():
             password
         ):
         login_url = "https://v.ruc.edu.cn/auth/login"
-        headers = self.get_headers()
+        headers = get_headers("vruc")
         src = requests.get(login_url, headers=headers).text
 
         soup = BeautifulSoup(src, 'html.parser')
@@ -30,34 +49,20 @@ class VRUCCrawler():
 
         session = requests.Session()
         session.post(url=login_url, data=json.dumps(data), headers=headers)
+
         self.session = session
-
-
-    def get_headers(self):
-        user_agent_pool = [
-            'Mozilla/5.0 (X11; CrOS i686 4319.74.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36',
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10',
-            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:23.0) Gecko/20131011 Firefox/23.0',
-            'Mozilla/5.0 (Windows; U; Windows NT 6.0; hu-HU) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
-        ]
-        headers = {
-            "User-Agent": random.choice(user_agent_pool),
-            "Accept": "application/xhtml+xml,application/json,text/plain,*/*"
-        }
-        return headers
+        self.headers = headers
 
 
     def get(self, url, headers=None, **kargs):
         if headers is None:
-            headers = self.get_headers()
+            headers = self.headers
         return self.session.get(url, headers=headers, **kargs)
 
 
     def post(self, url, headers=None, **kargs):
         if headers is None:
-            headers = self.get_headers()
+            headers = self.headers
         return self.session.post(url, headers=headers, **kargs)
 
 
@@ -65,23 +70,18 @@ class VRUCCrawler():
 class JWCrawler(VRUCCrawler):
     def __init__(self, student_id, password):
         super().__init__(student_id, password)
-        response = super().post("https://jw.ruc.edu.cn/secService/logout?resourceCode=resourceCode&apiCode=framework.sign.controller.SignController.logout")
+        headers = get_headers("jw")
+        response = super().post("https://jw.ruc.edu.cn/secService/logout?resourceCode=resourceCode&apiCode=framework.sign.controller.SignController.logout", headers=headers)
         redirect_uri = json.loads(response.text)["data"]
         # the second last history set the "token" cookie
         response = super().get(redirect_uri)
-        self.token = self.session.cookies.get_dict()["token"]
+        headers["TOKEN"] = self.session.cookies.get_dict()["token"]
 
-
-    def get_headers(self):
-        headers = super().get_headers()
-        # add TOKEN which is necessary for jw
-        if hasattr(self, "token"):
-            headers["TOKEN"] = self.token
-        return headers
+        self.headers = headers
 
 
     def get_grades(self):
-        headers = self.get_headers()
+        headers = self.headers
         headers["Content-Type"] = 'application/json'
         page = {
             "pageIndex": 1,
